f3d022ba1f064fa3864940488f8d24a53c9ecb7d
Mitchel Baker <mitchthebaker@Mitchels-MacBook-Pro.local>
2022-07-29 12:43:16 -0700

    added ability to sort asc/desc for both title/scheduledStartTime properties in pages/shows.js; will need to refactor and provide styling for buttons in next update

--- pages/shows.js
+++ pages/shows.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import Layout from '@c/Layout'
 import { Grid, Card } from '@c/Grid'
 import { Title } from '@c/Title'
 import { getAllShows } from '@l/graphcms'
+import { sortByProperty } from '@l/utils'
 
-export default function Shows({ shows }) {
+export default function Shows({ data }) {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => setShows(data), [])
+
+  const sortShows = (property) => {
+    /**
+     * Arrays in JS are referenced types, so even though sorting takes place it doesn't 
+     * change the reference to the original array. 
+     * 
+     * Solution here is to use spread operator inside new Array.
+     */
+    setShows([...shows.sort(sortByProperty(property))])
+  }
+
   return (
     <Layout title="next-graphcms-shows / Shows">
       <Title>Shows</Title>
+
+      <button onClick={() => sortShows("title")}> Sort by title ascending </button>
+      <button onClick={() => sortShows("-title")}> Sort by title descending </button>
+
+      <button onClick={() => sortShows("scheduledStartTime")}> Sort by scheduledStartTime ascending </button>
+      <button onClick={() => sortShows("-scheduledStartTime")}> Sort by scheduledStartTime descending </button>
+
       <Grid>
         {shows.map(show => (
           <Card href={`/show/${show.slug}`} header={show.title} key={show.id}>
@@ -19,8 +42,8 @@ export default function Shows({ shows }) {
 }
 
 export async function getServerSideProps() {
-  const shows = (await getAllShows()) || []
+  const data = (await getAllShows()) || []
   return {
-    props: { shows },
+    props: { data },
   }
 }
