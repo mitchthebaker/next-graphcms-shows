e7fa488c916b166412bf7c6fc37ed573fd9f8d70
Mitchel Baker <mitchthebaker@Mitchels-MacBook-Pro.local>
2022-07-30 00:23:17 -0700

    removed Artist components from /pages/show/[slug].js; created new page for artists

--- pages/show/[slug].js
+++ pages/show/[slug].js
@@ -1,44 +1,10 @@
-import ReactMarkdown from 'react-markdown'
-import styled from 'styled-components'
 import Layout from '@c/Layout'
 import { Link } from '@c/Link'
 import FlexyRow from '@c/FlexyRow'
 import { Title } from '@c/Title'
 import { getShowBySlug } from '@l/graphcms'
 import { formatUSD, formatDate } from '@l/utils'
-
-const Markdown = styled(ReactMarkdown)`
-  img {
-    width: 100%;
-    border-radius: 20px;
-    border: 4px solid currentColor;
-  }
-`
-
-const ArtistName = styled.h2`
-  text-align: center;
-`
-
-const ArtistPhoto = styled.div`
-  background-image: url(${(p) => p.imageUrl});
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 200px;
-  height: 200px;
-  border-radius: 100px;
-  border: 4px solid currentColor;
-  margin: 0 auto;
-`
-
-const Portrait = ({ images = [] }) => {
-  if (images.length > 0) {
-    const img = images[0]
-    return (
-      <ArtistPhoto imageUrl={img.url} />
-    )
-  }
-  return null
-}
+import { Markdown, ArtistName, Portrait } from '../artist/[slug]'
 
 export default function Shows({ show }) {
   return (
